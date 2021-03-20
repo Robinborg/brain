86ff28c4f84a2cfcefecdee2971f99fc1d651847
Robinborg <robin.borgatrom@gmail.com>
2021-03-20 19:35:46 +0100

    Setup and installing packages

---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib as mpl
+from tkinter import *
+import beautifulsoup4 as bs4
+
+'''The goal of this program is to create a secondary brain. This program will
+function for notetaking, saving links and fetching the important information,
+saving the data in a database which can be formed to the users liking and
+plotting the information for further learning'''
+
